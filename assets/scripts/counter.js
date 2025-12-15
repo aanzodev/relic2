555c5afb686668c2e702134917ebc8378e291107
Ayden <milohernandez285@gmail.com>
2025-12-14 19:18:23 -0600

    Update counter.js

--- assets/scripts/counter.js
+++ assets/scripts/counter.js
@@ -1,28 +1,13 @@
-  const firebaseConfig = {
-    apiKey: "AIzaSyC7zhMX7XGduwwtV7aH_ehON2eHI5jUenk",
-    authDomain: "relic-ultimate.firebaseapp.com",
-    databaseURL: "https://relic-ultimate-default-rtdb.firebaseio.com",
-    projectId: "relic-ultimate",
-    storageBucket: "relic-ultimate.firebasestorage.app",
-    messagingSenderId: "1047341587934",
-    appId: "1:1047341587934:web:0f40ca1d7a4ca72715114f",
-};
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-// Get database reference
 const database = firebase.database();
 
-console.log('Firebase initialized successfully');
+console.log('Hopefully this fixses it');
 
 (function() {
-  console.log('Firebase real-time user counter initialized');
+  console.log('Firebase is loaded');
 
   // Check if Firebase is available
   if (typeof firebase === 'undefined' || !firebase.database) {
-    console.error('Firebase not loaded! Using fallback counter.');
+    console.error('Firebase is not loaded');
     useFallbackCounter();
     return;
   }
