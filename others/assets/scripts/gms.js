cafbf550fb71d76b5c2e4184b8c89b5074259c14
Ayden <milohernandez285@gmail.com>
2025-12-18 00:23:38 -0600

    Update gms.js

--- others/assets/scripts/gms.js
+++ others/assets/scripts/gms.js
@@ -206,11 +206,12 @@ const apps = [
   { name: "Sandstone", image: "others/assets/apps/images/sandstone.jpg", url: "others/assets/apps/sandstone (1).html" },
   { name: "Silk", image: "others/assets/apps/images/silk.jpg", url: "others/assets/apps/clsilk (1).html" },
   { name: "Soundboard", image: "others/assets/apps/images/soundboard.png", url: "others/assets/apps/soundboard.html" },
-  { name: "Soundcloud", image: "others/assets/apps/images/soundcloud.png", url: "others/assets/apps/soundcloud (1).html" },
+  { name: "Soundcloud", image: "others/assets/apps/images/soundcloud.png", url: "href='public/active/index.html?autofill=https://soundcloud.com'" },
   { name: "Spotify (SOON)", image: "others/assets/apps/images/spotify.png", url: "about:blank"}, 
   { name: "Vscode", image: "others/assets/apps/images/vscode.jpeg", url: "others/assets/apps/vscode.html" },
   { name: "YouTube", image: "others/assets/apps/images/youtube.png", url: "/others/assets/apps/yt.html"},
 ];
+
 
 // SCRAP
 const websites = [
