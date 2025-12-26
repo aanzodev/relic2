2f9d9f979dd1e15929fe4663943692d6f50fdabc
Ayden <milohernandez285@gmail.com>
2025-12-26 02:40:50 +0000

    Fixed Scramjet proxy setup with bare-mux and epoxy

--- sw.js
+++ sw.js
@@ -1,43 +1,35 @@
-importScripts(
-  "/public/vu/vu.bundle.js",
-)
-importScripts("/public/vu/vu.config.js")
-importScripts("/public/vu/vu.sw.js")
+importScripts("/public/vu/vu.bundle.js");
+importScripts("/public/vu/vu.config.js");
+importScripts("/public/vu/vu.sw.js");
 importScripts("/public/scram/scramjet.all.js");
-
-
+importScripts("/public/baremux/bare.cjs"); // ADD THIS LINE
 
 if (navigator.userAgent.includes("Firefox")) {
   Object.defineProperty(globalThis, "crossOriginIsolated", {
     value: true,
     writable: true,
-  })
+  });
 }
 
-const vu = new UVServiceWorker()
+const vu = new UVServiceWorker();
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
 
 self.addEventListener("install", () => {
-  self.skipWaiting()
-})
+  self.skipWaiting();
+});
 
 async function handleRequest(event) {
-  await scramjet.loadConfig()
+  await scramjet.loadConfig();
   if (scramjet.route(event)) {
-    return scramjet.fetch(event)
+    return scramjet.fetch(event);
   }
-
 
   if (vu.route(event)) return await vu.fetch(event);
     
-  return await fetch(event.request)
+  return await fetch(event.request);
 }
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event))
-})
-
-self.addEventListener("install", (self) => {
-	self.skipWaiting()
-})
+  event.respondWith(handleRequest(event));
+});
